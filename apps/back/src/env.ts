771466ffb48c900988b94131c1e080e4ebfbaf75
Alexandre Sanchez <contact@alexandresanchez.xyz>
2023-04-29 07:53:12 +0200

    feat(env): added t3 env for validation - Replaced base T3 setup by it for front - Added validation to the back

---
+++ apps/back/src/env.ts
@@ -0,0 +1,11 @@
+import { createEnv } from "@t3-oss/env-core"
+import { z } from "zod"
+
+export const env = createEnv({
+	clientPrefix: "PUBLIC_",
+	client: {},
+	server: {
+		DATABASE_URL: z.string().min(1)
+	},
+	runtimeEnv: process.env
+})
--- apps/back/src/main.ts
+++ apps/back/src/main.ts
@@ -2,11 +2,12 @@ import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 import { CustomPrismaService } from "nestjs-prisma"
 import { PrismaClient } from "database"
+import "./env"
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	const prismaService: CustomPrismaService<PrismaClient> = app.get("PrismaService")
-	prismaService.enableShutdownHooks(app)
+	void prismaService.enableShutdownHooks(app)
 
 	// Enable cors
 	app.enableCors()
